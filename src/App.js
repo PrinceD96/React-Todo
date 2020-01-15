a90111bf2bc414baee8fda215429bd04b27df6b9
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-01-15 00:05:45 -0500

    Dependencies install. Initial state setup.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React from "react";
+
+const todo = [
+	{
+		task: "Fork and clone the project",
+		id: "1528817077098",
+		completed: "false"
+	},
+	{
+		task: "Install node modules",
+		id: "9876547077098",
+		completed: "false"
+	},
+	{
+		task: "Start the development server",
+		id: "0987654321098",
+		completed: "false"
+	}
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+	constructor(props) {
+		super();
+		this.state = {
+			todoList: todo
+		};
+	}
+	// you will need a place to store your state in this component.
+	// design `App` to be the parent component of your application.
+	// this component is going to take care of state, and any change handlers you need to work with your state
+
+	render() {
+		return (
+			<div>
+				<h2>Welcome to your Todo App!</h2>
+			</div>
+		);
+	}
 }
 
 export default App;
