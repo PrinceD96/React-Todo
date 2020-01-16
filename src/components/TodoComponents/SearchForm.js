4b9004ae3f75922f2b8cd90c5c6e0901b1b45d88
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-01-16 00:52:38 -0500

    Search Functionality completedgit add .!

--- src/components/TodoComponents/SearchForm.js
+++ src/components/TodoComponents/SearchForm.js
@@ -1,44 +1,18 @@
 import React, { useState, useEffect } from "react";
 
-const SearchForm = ({ todo }) => {
-	const [searchTerm, setSearchTerm] = useState("");
-	const [searchResults, setSearchResults] = useState(todo);
-
-	useEffect(() => {}, [searchTerm]);
-
-	const handleChange = e => {
-		setSearchTerm(e.target.value);
-
-		const results = todo.filter(todo => {
-			return todo.task.toLowerCase().includes(searchTerm.toLowerCase());
-		});
-
-		setSearchResults(results);
-	};
-
-	const handleSubmit = e => {
-		e.preventDefault();
-	};
-
-	return (
-		<>
-			<form onSubmit={handleSubmit} className='search'>
-				<input
-					id='task'
-					type='text'
-					name='task'
-					placeholder='Search by task name'
-					onChange={handleChange}
-					value={searchTerm}
-				/>
-			</form>
-			<div>
-				{searchResults.map(todo => {
-					return <p key={todo.id}>{todo.task}</p>;
-				})}
-			</div>
-		</>
-	);
-};
+const SearchForm = ({ handleSearch, state }) => (
+	<>
+		<div className='search'>
+			<input
+				id='task'
+				type='text'
+				name='task'
+				placeholder='Search by task name'
+				onChange={handleSearch}
+				value={state.searchTerm}
+			/>
+		</div>
+	</>
+);
 
 export default SearchForm;
